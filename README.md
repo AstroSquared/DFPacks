bfa0306fa5533bdff7617c857b65baee02e06811
AstroSquared <jackwatch24@gmail.com>
2023-09-08 22:48:24 -0400

    i hope the CI works :bangbang:

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DFPacks
+
+Template pack is in `.template` folder.
+
+CI automatically builds every folder that doesn't start with a `.`.
---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir .output
+
+for dir in */; do
+    if [[ ${dir:0:1} != "." ]]; then
+        echo "Compiling resource pack: $dir"
+        zip -r out.zip "./$dir"
+        mv out.zip "./.output/$dir.zip"
+    fi
+done
