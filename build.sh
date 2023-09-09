39bd1656e696e4e5e158e57fd094da8883cba36c
AstroSquared <jackwatch24@gmail.com>
2023-09-08 22:59:56 -0400

    3

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 mkdir .output
 
 for dir in */; do
-    if [[ $dir == .* ]]; then
+    if [[ $dir == ".*" ]]; then
         echo "Compiling resource pack: $dir"
         zip -r out.zip "./$dir"
         mv out.zip "./.output/$dir.zip"
